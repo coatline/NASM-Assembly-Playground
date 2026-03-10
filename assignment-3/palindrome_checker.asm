870f723bf9cb158ed09471112d3b2114e3c432bc
BrylerAtSchool <bncastlebe42@tntech.edu>
2026-03-10 15:41:55 -0500

    Started Palindrome Checker for Assignment 3

---
+++ assignment-3/palindrome_checker.asm
@@ -0,0 +1,63 @@
+SYS_WRITE equ 4
+SYS_READ equ 3
+SYS_EXIT equ 1
+STDOUT equ 1
+STDIN equ 0
+NL equ 10
+
+section .data
+    prompt db "Please enter a string:", NL
+    prompt_len equ $ - prompt
+    msg_is_pal db "It is a palindrome", NL
+    msg_is_pal_len equ $ - msg_is_pal
+    msg_not_pal db "It is NOT a palindrome", NL
+    msg_not_pal_len equ $ - msg_not_pal
+
+section .bss
+    buffer resb 1024
+
+section .text
+    global _start
+
+_start:
+.main_loop:
+    ; Print the prompt
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, prompt
+    mov edx, prompt_len
+    int 0x80
+
+    ; Read the input
+    mov eax, SYS_READ
+    mov ebx, STDIN
+    mov ecx, buffer
+    mov edx, 1024
+    int 0x80
+
+    ; does the value of buffer equal a new line? if so, exit_program
+    cmp byte [buffer], NL
+    je .exit_program
+
+    mov esi, eax    ; save the read length in esi
+    dec esi         ; subtract 1 to ignore the new line at the end
+
+    push esi        ; push 2nd argument length
+    push buffer     ; push 1st argument buffer address
+
+    call is_palindrome
+
+    ; esp is a special register that always points to the top of the stack.
+
+.print_false:
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, msg_not_pal
+    mov edx, msg_not_pal_len
+    int 0x80
+    jmp .main_loop
+
+.exit_program:
+    mov eax, SYS_EXIT
+    xor ebx, ebx
+    int 0x80
